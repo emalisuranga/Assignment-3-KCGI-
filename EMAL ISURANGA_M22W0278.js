8b81f79957e5fc692359af2a913e77eb6750a8b2
rmsachithra <rmsachithra@icloud.com>
2022-12-04 11:35:29 +0900

    Make Player class and constructor

---
+++ EMAL ISURANGA_M22W0278.js
@@ -0,0 +1,16 @@
+// Name : EMAL ISURANGA
+// USER ID : M22W0278
+
+class Player {
+    
+    constructor (name) {
+        this.playerName = name;
+        this.playLife = 3;
+    }
+}
+
+const player = new Player("EMAL");
+
+
+
+
