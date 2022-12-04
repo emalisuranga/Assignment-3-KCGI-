6a5bcb84ab282ccaef13d4e89e027d6fba71082a
rmsachithra <rmsachithra@icloud.com>
2022-12-04 11:44:10 +0900

    make function for find larger sum of object
    part-2 this part use for calculate to sum of each object and find larger value

--- EMAL ISURANGA_M22W0278.js
+++ EMAL ISURANGA_M22W0278.js
@@ -18,6 +18,7 @@ class Player {
         }
         return this.playLife == 0 ?  console.log("game over") : this.playLife
     }
+     
 }
 
 const player = new Player("EMAL");
@@ -33,4 +34,17 @@ console.log(player.getLives()); // outputs 0
 
 
 
+// PART #2
 
+function getObjectSum(numberObject1,numberObject2){
+    let sum1 = 0;
+    let sum2 = 0;
+    Object.values(numberObject1).forEach(val => {
+        sum1 += val;
+    });
+    Object.values(numberObject2).forEach(val => {
+        sum2 += val;
+    });
+    
+    return Math.max(sum1,sum2);
+}
